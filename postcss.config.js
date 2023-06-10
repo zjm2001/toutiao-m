c349497b4b147d52ada583c8d501dbc9f29dcb4f
zjm <2215539769@qq.com>
2023-06-10 15:43:02 +0800

    完成使用vw配置网页

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable eol-last */
+/* eslint-disable indent */
+module.exports = {
+    plugins: {
+      'postcss-px-to-viewport': {
+        viewportWidth: 375
+      }
+    }
+  }
--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <h1>ash的卡和肯定会</h1>
     <i class="iconfont icon-shouye "></i>
     <van-button type="primary">vant组件的可以使用 unplugin-vue-components 插件，它可以自动引入组件，并按需引入组件的样式</van-button>
+    <h1 class="text">我大好多</h1>
   </div>
 </template>
 <script>
@@ -13,4 +14,10 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.text{
+  width: 375px;
+  height: 100px;
+  background-color: aqua;
+}
+</style>
